4ab3d32c7370a3d8c157a404532caaefb7f7423b
Sherlockboy <kabilovhurshid5@gmail.com>
2020-08-15 00:35:15 +0500

    Updates

---
+++ precache-manifest.9d98972233a492c163fd4a6e8b844214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0566b51d44454b748af0d7c656bb3959",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "cecc391971463d61713f",
+    "url": "/first-react-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f43d2e9978a560389407",
+    "url": "/first-react-app/static/css/main.de3a120a.chunk.css"
+  },
+  {
+    "revision": "cecc391971463d61713f",
+    "url": "/first-react-app/static/js/2.ccb4a6b3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/first-react-app/static/js/2.ccb4a6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f43d2e9978a560389407",
+    "url": "/first-react-app/static/js/main.5c067f9e.chunk.js"
+  },
+  {
+    "revision": "a66f119331605d283c77",
+    "url": "/first-react-app/static/js/runtime-main.642f834c.js"
+  }
+]);
